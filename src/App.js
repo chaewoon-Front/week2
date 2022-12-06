fe5cbd1c0461778e3cd7aa6126a1cddf7a4add60
chaewoon-Front <treechae38@naver.com>
2022-12-06 10:16:09 +0900

    dispatch 연습

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // src/App.js
 
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useDispatch } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  const counterStore = useSelector((state) => state);
-  const number = useSelector((state) => state.counter.number);
-  console.log(counterStore);
-  console.log(number);
-
-  return <div></div>;
+  const dispatch = useDispatch(); // dispatch 생성
+  return (
+    <div>
+      <button
+        // 이벤트 핸들러 추가
+        onClick={() => {
+          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+          dispatch({ type: "PLUS_ONE" });
+        }}
+      >
+        + 1
+      </button>
+    </div>
+  );
 };
 
 export default App;
