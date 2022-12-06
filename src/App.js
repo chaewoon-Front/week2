3193985b064f0369a4451965e99601287e6884a4
chaewoon-Front <treechae38@naver.com>
2022-12-06 10:39:13 +0900

    모듈 만들기 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 // src/App.js
 
 import React from "react";
-import { useDispatch } from "react-redux"; // import 해주세요.
+import { useSelector } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  const dispatch = useDispatch(); // dispatch 생성
-  return (
-    <div>
-      <button
-        // 이벤트 핸들러 추가
-        onClick={() => {
-          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
-          dispatch({ type: "PLUS_ONE" });
-        }}
-      >
-        + 1
-      </button>
-    </div>
-  );
+  const counterStore = useSelector((state) => state);
+  console.log(counterStore);
+
+  return <div></div>;
 };
 
 export default App;
