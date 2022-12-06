43cbb187fd5bb02006c5d6ac5eae6cfa734cf4d1
chaewoon-Front <treechae38@naver.com>
2022-12-06 15:35:53 +0900

    action creatore 연습

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@
 import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
+import { minusOne, plusOne } from "./redux/modules/counter";
 
 const App = () => {
   const dispatch = useDispatch();
+  const number = useSelector((state) => state.counter.number);
+  console.log(number);
   return (
     <div>
+      {number}
       <button
         onClick={() => {
-          dispatch({ type: "PLUS_ONE" });
+          dispatch(plusOne());
         }}
       >
         +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch(minusOne());
+        }}
+      >
+        -1
       </button>
     </div>
   );
